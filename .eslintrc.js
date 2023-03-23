05c9a4da2e9b4315825d6d6c9a6104cc68f02d07
sachinsaitej7 <63497257+sachinsaitej7@users.noreply.github.com>
2023-03-24 05:21:35 +0530

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
-  extends: ["react-app"],
+  extends: ["react-app", "react-app/jest"],
   rules: {},
   env: {
     NODE_ENV,
